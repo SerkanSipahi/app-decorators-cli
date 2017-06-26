158e4b502f25eb4eabcda0a23f952cba2b5a4b14
Serkan Sipahi <serkan.sipahi@yahoo.de>
2017-06-26 22:18:13 +0200

    refactor; reduce complexity

--- bundle.go
+++ bundle.go
@@ -3,15 +3,11 @@ package main
 import (
 	"log"
 	"os"
-	"os/exec"
 )
 
 func bundle(appPath string) error {
 
-	var (
-		err error
-		cmd *exec.Cmd
-	)
+	var err error
 
 	// change to component
 	if err = os.Chdir(appPath); err != nil {
@@ -19,27 +15,15 @@ func bundle(appPath string) error {
 	}
 
 	// compile files
-	err = compile("src", "lib", true, true)
+	err = compile("src", "lib", false, true)
 	if err != nil {
 		panic(err)
 	}
 
 	// bundle
-	// node_modules/.bin/jspm bundle 'src/**/* - app-decorators - [src/**/*]' deps.js --config jspm.config.json --minify
-	cmd = exec.Command("node_modules/.bin/jspm", "bundle",
-		"src/index", "-", "app-decorators",
-		"lib/index.js",
-		"--minify",
-		// FIXME: if --no-mangle the build doesnt work well! it convert e.g component name from collapsible to e.g b
-		"--no-mangle",
-		"--skip-source-maps",
-	)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	err = cmd.Run()
+	err = build("src/index.js", "lib/index.js", "default", true, true)
 	if err != nil {
-		log.Fatalln("Cant bundle component: "+appPath, err)
+		panic(err)
 	}
 
 	return nil
